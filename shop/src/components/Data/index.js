7d0efa1197114707f346e06e689cecfe097d9c08
AnkurYadav619 <yadavankur619@gmail.com>
2019-01-07 15:14:46 +0530

    Update index.js

---
+++ shop/src/components/Data/index.js
@@ -0,0 +1,225 @@
+const products = [
+  {
+    id: 1,
+    name: "Radeon Pro WX4100",
+    description: "AMD Graphics Card",
+    price: 398.55,
+    group: "newProducts",
+    type: "graphic card",
+    img: "https://i.ibb.co/8x930KV/AMD1.jpg",
+    inCart: false,
+    category: "graphics"
+  },
+  {
+    id: 2,
+    name: "AORUS RX 580",
+    description: "AMD Graphics Card",
+    price: 424.11,
+    group: "specials",
+    type: "graphic card",
+    img: "https://i.ibb.co/2hBm61X/AMD2.png",
+    inCart: false,
+    category: "graphics"
+  },
+  {
+    id: 3,
+    name: "Gigabyte GV-N1050G1",
+    description: "Gigabyte Graphics",
+    price: 120.92,
+    group: "newProducts ",
+    type: "graphic card",
+    img: "https://i.ibb.co/tM039LT/gb.jpg",
+    inCart: false,
+    category: "graphics"
+  },
+  {
+    id: 4,
+    name: "MSI GeForce GTX1080",
+    description: "MSI Graphics Card",
+    price: 1204.12,
+    group: "specials",
+    type: "graphic card",
+    img: "https://i.ibb.co/7yHf7xx/msi.jpg",
+    inCart: false,
+    category: "graphics"
+  },
+/*  {
+    id: 5,
+    name: "",
+    description: "Light Grey Shirt for Women",
+    price: 4.99,
+    group: "",
+    type: "t",
+    img: "https://i.ibb.co/GRMD00N/monitor2.jpg",
+    inCart: false,
+    category: ""
+  },
+  {
+    id: 6,
+    name: "",
+    description: "",
+    price: 19.99,
+    group: "",
+    type: "",
+    img: "https://i.ibb.co/0XBCKWJ/benq.jpg",
+    inCart: false,
+    category: ""
+  },
+  {
+    id: 7,
+    name: "",
+    description: "",
+    price: 6.00,
+    group: "",
+    type: "",
+    img: "https://i.ibb.co/s3zYywX/lg.jpg",
+    inCart: false,
+    category: ""
+  },
+  {
+    id: 8,
+    name: "",
+    description: "",
+    price: 14.99,
+    group: "",
+    type: "",
+    img: "https://i.ibb.co/KxvWyrp/mouse.jpg",
+    inCart: false,
+    category: ""
+  },
+  {
+    id: 9,
+    name: "",
+    description: "",
+    price: 20.99,
+    group: "",
+    type: "",
+    img: "https://i.ibb.co/5kZBbhH/viotek.jpg",
+    inCart: false,
+    category: ""
+  },
+  {
+    id: 10,
+    name: "",
+    description: "",
+    price: 4.99,
+    group: "",
+    type: "",
+    img: "https://i.ibb.co/Y2jc1FN/ram.jpg",
+    inCart: false,
+    category: ""
+  },
+  {
+    id: 11,
+    name: "",
+    description: "",
+    price: 45.99,
+    group: "",
+    type: "",
+    img: "https://i.ibb.co/MGCDL7g/ssd.jpg",
+    inCart: false,
+    category: ""
+  },
+  {
+    id: 12,
+    name: "",
+    description: "",
+    price: 14.99,
+    group: "",
+    type: "",
+    img: "",
+    inCart: false,
+    category: "accessories"
+  },
+  {
+    id: 13,
+    name: "",
+    description: "",
+    price: 10.99,
+    group: "",
+    type: "",
+    img: "https://i.ibb.co/Z25hzFn/toshiba.jpg",
+    inCart: false,
+    category: ""
+  },
+  {
+    id: 14,
+    name: "",
+    description: "",
+    price: 14.99,
+    group: "",
+    type: "",
+    img: "https://i.ibb.co/jDkjC4j/seagate.jpg",
+    inCart: false,
+    category: ""
+  },
+  {
+    id: 15,
+    name: "",
+    description: "",
+    price: 20.99,
+    group: "",
+    type: "",
+    img: "https://i.ibb.co/5YMW8wN/mouse3.jpg",
+    inCart: false,
+    category: ""
+  },
+  {
+    id: 16,
+    name: "",
+    description: "",
+    price: 13.99,
+    group: "",
+    type: "",
+    img: "https://i.ibb.co/mzNTMSv/mouse2.jpg",
+    inCart: false,
+    category: ""
+  },
+  {
+    id: 17,
+    name: "",
+    description: "",
+    price: 13.99,
+    group: "",
+    type: "",
+    img: "https://i.ibb.co/16FmTrY/artis.jpg",
+    inCart: false,
+    category: "accessories"
+  },
+  {
+    id: 18,
+    name: "",
+    description: "",
+    price: 9.99,
+    group: "",
+    type: "",
+    img: "https://i.ibb.co/m5Qw6PM/redgear-Kb.jpg",
+    inCart: false,
+    category: ""
+  },
+  {
+    id: 19,
+    name: "",
+    description: "",
+    price: 35.99,
+    group: "",
+    type: "",
+    img: "https://i.ibb.co/hsgkfWC/ups2.jpg",
+    inCart: false,
+    category: "accessories"
+  },
+  {
+    id: 20,
+    name: "",
+    description: "",
+    price: 15.99,
+    group: "",
+    type: "",
+    img: "https://i.ibb.co/xMjTKWw/ups.jpg",
+    inCart: false,
+    category: "accessories"
+  },
+*/
+];
+
+export default products;
