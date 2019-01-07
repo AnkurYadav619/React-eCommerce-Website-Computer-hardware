6437def0bf208d39d869d4a5305cec7c738898ce
AnkurYadav619 <yadavankur619@gmail.com>
2019-01-07 15:14:57 +0530

    Update index.js

--- shop/src/index.js
+++ shop/src/index.js
@@ -3,7 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import App from './components/App';
 import BaseLayout from './components/BaseLayout';
+import Cart from './components/Cart';
+import ShowProduct from './components/ShowProduct';
+
+
 
 import 'bootstrap/dist/css/bootstrap.css';
 import * as serviceWorker from './serviceWorker';
@@ -12,7 +18,9 @@ ReactDOM.render(
   <BrowserRouter>
     <BaseLayout>
       <Switch>
-
+        <Route exact path="/" component={App} />
+        <Route path="/cart" component={Cart} />
+        <Route exact path="/products/:id" component={ShowProduct} />
 
 
       </Switch>
