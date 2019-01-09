4ede79ab0a0ef8451d1df350d9212795b1ee1162
AnkurYadav619 <yadavankur619@gmail.com>
2019-01-09 11:21:31 +0530

    Create index.js

---
+++ shop/src/components/Specials/index.js
@@ -0,0 +1,17 @@
+//Dependencies
+import React from 'react';
+//Internals
+import SpecialItems from './SpecialItems';
+import './index.css';
+
+const SpecialProducts = () => (
+  <div className="special-products">
+    <div className="specialProducts-title">
+      <h4>Featured Products</h4>
+    </div>
+    <SpecialItems />
+  </div>
+);
+
+
+export default SpecialProducts;
