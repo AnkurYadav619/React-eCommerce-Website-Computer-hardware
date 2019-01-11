6e78200f142319a208966ec7a674c70efb06d6f2
AnkurYadav619 <yadavankur619@gmail.com>
2019-01-11 13:15:41 +0530

    Create index.js

---
+++ shop/src/components/Faq/index.js
@@ -0,0 +1,21 @@
+// Dependencies
+import React from 'react';
+// Internals
+import './styles.css';
+
+
+const Faq = () => (
+  <div className="faq">
+    <div className="faq-title">
+      <h2>FAQs</h2>
+    <div className="faq-data">
+      <p> <li>Question 1, Lorem Ipsum is simply dummy text of the printing and typesetting industry.</li>
+          <li>Answer 1:Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled.
+          </li>
+      </p>
+    </div>
+    </div>
+  </div>
+);
+
+export default Faq;
