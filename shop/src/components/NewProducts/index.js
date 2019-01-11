27dd69387854bad55e7d6af14c25e68a135dfd91
AnkurYadav619 <yadavankur619@gmail.com>
2019-01-11 13:16:09 +0530

    Update index.js

--- shop/src/components/NewProducts/index.js
+++ shop/src/components/NewProducts/index.js
@@ -5,6 +5,7 @@ import NewItems from './NewItems';
 import './index.css';
 
 const NewProducts = () => (
+
   <div className="new-products">
     <div className="newProducts-title">
       <h4>New Products</h4>
