4b1e49dcbd2a27de5e8b27212b82de1da6f4e29a
AnkurYadav619 <yadavankur619@gmail.com>
2019-01-11 13:15:32 +0530

    Create index.js

---
+++ shop/src/components/Contact/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import './styles.css';
+export default class Form extends React.Component{
+  state={
+    name:'',
+    email:'',
+    message:'',
+  };
+
+onSubmit=(e)=>{
+  e.preventDefault();
+  console.log(this.state)
+  this.setState({
+    name:'',
+    email:'',
+    message:'',
+  });
+};
+
+render(){
+  return(
+      <form className="box">
+        <h1>CONTACT</h1>
+        <input type="text" placeholder='Name'
+        value={this.state.name}
+        onChange={e=> this.setState({ name: e.target.value})}
+        />
+        <input type="text" placeholder='hello@world.com'
+        value={this.state.email}
+        onChange={e=> this.setState({ email: e.target.value})}
+        />
+        <input type="text" placeholder='Message'
+        value={this.state.message}
+        onChange={e=> this.setState({ message: e.target.value})}
+        />
+        <button onClick={e=>this.onSubmit(e)}>Submit</button>
+      </form>
+    );
+  }
+}
